0894e7d6ca1bda8b72c3f8f7a6d4bf09c52f6591
coseo12 <momo_12@naver.com>
2020-09-27 17:50:22 +0900

    Updates

---
+++ precache-manifest.a17e92eea3a1ad788b156d5fe8ab7dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98614b93ce6d306019f6cc8cc1d6a400",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "9e6a04c1c66cfcf9f0bd",
+    "url": "/nwitter/static/css/main.51fba151.chunk.css"
+  },
+  {
+    "revision": "244c459c9969f64554e3",
+    "url": "/nwitter/static/js/2.0599c11a.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.0599c11a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e6a04c1c66cfcf9f0bd",
+    "url": "/nwitter/static/js/main.c1c14fa3.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
